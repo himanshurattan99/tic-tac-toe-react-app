7adbf762e7f2fb878a0a1b396487d56544555e78
himanshu <r99.himanshu@gmail.com>
2025-01-23 18:45:24 +0530

    feat - create Tic Tac Toe game page
    style - add CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Cell from './components/Cell'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cells, setCells] = useState(Array(9).fill(''));
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="h-screen bg-sky-200 text-blue-700 flex flex-col justify-center items-center gap-4">
+        <h2 className="text-2xl font-bold">Tic Tac Toe</h2>
+
+        <div className="p-2 bg-sky-50 border-[0.2rem] border-blue-500 rounded-lg grid grid-cols-3 grid-rows-3 gap-2">
+          {cells.map((_, index) => (
+            <Cell key={index} />
+          ))}
+        </div>
+
+        <div className="w-full flex flex-col justify-center items-center gap-3">
+          <button type="reset" className="px-3 py-1 bg-blue-500 hover:bg-green-500 rounded text-gray-100 cursor-pointer">Reset</button>
+
+          <div className="px-8 py-3 bg-sky-50 border-2 border-blue-500 rounded-xl text-lg">Play</div>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/Cell.jsx
@@ -0,0 +1,11 @@
+function Cell() {
+    return (
+        <>
+            <div className="w-[7.5vw] h-[7.5vw] bg-green-100 hover:bg-green-300 border-2 border-blue-500 hover:border-gray-100 rounded-lg hover:text-gray-100 flex justify-center items-center cursor-pointer">
+                <span className="text-4xl" ></span>
+            </div>
+        </>
+    )
+}
+
+export default Cell
